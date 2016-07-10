0547231158a3e59518bb3867b583fc57d3f5f101
Petter Arvidsson <petter.arvidsson@gmail.com>
2016-07-10 12:37:20 +0200

    Add message to get block factory

---
+++ src/main/java/konstructs/api/messages/GetBlockFactory.java
@@ -0,0 +1,7 @@
+package konstructs.api.messages;
+
+public class GetBlockFactory {
+    public final static GetBlockFactory MESSAGE = new GetBlockFactory();
+    private GetBlockFactory() {
+    }
+}
